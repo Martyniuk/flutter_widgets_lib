656afaa145eed024f02c8122b5c19651d6dab744
Vladimir Martyniuk <vl.martyniuk@gmail.com>
2019-11-19 22:46:14 +0200

    feat: bicycle app added

--- lib/practice/basic/bicycle_app.dart
+++ lib/practice/basic/bicycle_app.dart
@@ -39,10 +39,14 @@ class BicycleApp extends StatelessWidget {
                   children: <Widget>[
                     Align(
                         alignment: Alignment.topRight,
-                        child: Container(
-                          width: w * .9,
-                          height: h * .75,
-                          color: Color(0xFFFFC5C5),
+                        child: Hero(
+                          // for layout purpose - unigue tag will be 'tag'
+                          tag: 'tag',
+                          child: Container(
+                            width: w * .9,
+                            height: h * .75,
+                            color: Color(0xFFFFC5C5),
+                          ),
                         )),
                     Positioned(
                       top: 50,
@@ -74,10 +78,13 @@ class BicycleApp extends StatelessWidget {
                     Positioned(
                       top: h * .32,
                       left: -50,
-                      child: Image.asset(
-                        product.imgUrl,
-                        width: 370,
-                        height: 350,
+                      child: Hero(
+                        tag: product.name,
+                        child: Image.asset(
+                          product.imgUrl,
+                          width: 370,
+                          height: 350,
+                        ),
                       ),
                     ),
                   ],
@@ -86,7 +93,7 @@ class BicycleApp extends StatelessWidget {
             ),
           ),
           Padding(
-            padding: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.only(left: 30.0, right: 5.0),
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
@@ -105,7 +112,6 @@ class BicycleApp extends StatelessWidget {
                     fontSize: 35,
                     fontFamily: "Montserrat-M",
                     fontWeight: FontWeight.w900,
-                    letterSpacing: 1.2,
                   ),
                 ),
                 SizedBox(height: 15.0),
@@ -118,7 +124,164 @@ class BicycleApp extends StatelessWidget {
                     fontWeight: FontWeight.w500,
                   ),
                 ),
+                Padding(
+                  padding: const EdgeInsets.only(right: 30.0, top: 20.0),
+                  child: GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => ProductDetail(product: product),
+                        ),
+                      );
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: <Widget>[
+                        Icon(
+                          Icons.arrow_forward,
+                          size: 42.0,
+                        )
+                      ],
+                    ),
+                  ),
+                )
               ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProductDetail extends StatelessWidget {
+  final Product product;
+
+  ProductDetail({this.product});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Hero(
+            tag: 'tag',
+            child: Container(
+              width: double.infinity,
+              height: double.infinity,
+              color: Color(0xFFFFC5C5),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(left: 35.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Padding(
+                    padding: EdgeInsets.only(
+                        left: 18, right: 25, top: 50, bottom: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          product.type,
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontFamily: "Montserrat-B",
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: .5,
+                          ),
+                        ),
+                        Container(
+                          width: 45,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                          ),
+                          child: Icon(Icons.shopping_cart, size: 18),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Hero(
+                    tag: product.name,
+                    child: Image.asset(product.imgUrl, width: 280, height: 250),
+                  ),
+                  SizedBox(height: 50),
+                  Text(
+                    product.name,
+                    style: TextStyle(
+                      fontSize: 40,
+                      fontFamily: "Montserrat-B",
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: 1.5,
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 10.0),
+                    child: Text(
+                      "Top Fuel 9.9 XX1 AXS is top tier full auto"
+                      "suspension carbon mountain bike.",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: Text(
+                      "It's also spec's with hight-end RockShox"
+                      "suspension and lots of carbon-including"
+                      "the wheels, bars, and shift levels. You'll fly"
+                      "throught singletrack and rip descents on"
+                      "this bike.",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontFamily: "Montserrat",
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 30),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      Text(
+                        '\$${product.price}',
+                        style: TextStyle(
+                          fontSize: 40,
+                          fontFamily: "Montserrat-M",
+                          fontWeight: FontWeight.w700,
+                          letterSpacing: 1.5,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20.0),
+                        child: FlatButton(
+                          child: Text(
+                            'ADD TO CART',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontFamily: "Montserrat-M",
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.5,
+                              decoration: TextDecoration.underline,
+                            ),
+                          ),
+                          onPressed: () {},
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         ],
