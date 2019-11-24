9bce77e25513536f95e950833b2d07106e0b02c2
Vladimir Martyniuk <vl.martyniuk@gmail.com>
2019-11-24 17:03:30 +0200

    fix: drawer updated

--- lib/main.dart
+++ lib/main.dart
@@ -4,22 +4,22 @@ import 'package:flutter/material.dart';
 // Library of widgets I play with
 // import 'library/library.dart';
 // Basic dummy layouts
-import 'practice/basic/bankscreen.dart';
+// import 'practice/basic/bankscreen.dart';
 // import 'practice/basic/lampshop.dart';
-import 'practice/basic/travelapp.dart';
-import 'practice/basic/darkTravel.dart';
-import 'practice/basic/productRelease.dart';
-import 'practice/basic/smartHome.dart';
-import 'practice/basic/restaurant.dart';
-import 'practice/basic/travelApp2.dart';
-import 'practice/basic/story_app.dart';
-import 'practice/basic/onboarding_screens.dart';
-import 'practice/basic/bicycle_app.dart';
+// import 'practice/basic/travelapp.dart';
+// import 'practice/basic/darkTravel.dart';
+// import 'practice/basic/productRelease.dart';
+// import 'practice/basic/smartHome.dart';
+// import 'practice/basic/restaurant.dart';
+// import 'practice/basic/travelApp2.dart';
+// import 'practice/basic/story_app.dart';
+// import 'practice/basic/onboarding_screens.dart';
+// import 'practice/basic/bicycle_app.dart';
 
 // Advanced layouts
-import 'practice/medium/travel_ui/travel_ui.dart';
+// import 'practice/medium/travel_ui/travel_ui.dart';
 import 'practice/medium/save_animals/save_animals.dart';
-import 'practice/medium/tattoo/tattoo_app.dart';
+// import 'practice/medium/tattoo/tattoo_app.dart';
 
 void main() => runApp(MyApp());
 
@@ -29,27 +29,40 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
+      theme: ThemeData(
+        primaryColor: Color.fromRGBO(45, 45, 45, 1.0),
+      ),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     Widget _drawDrawer() {
       return Drawer(
         child: ListView(
-          // Important: Remove any padding from the ListView.
           padding: EdgeInsets.zero,
           children: <Widget>[
-            DrawerHeader(
-              child: Text('Drawer Header'),
-              decoration: BoxDecoration(
-                color: Colors.blue,
+            UserAccountsDrawerHeader(
+              accountName: Text(
+                "Vladimir Martyniuk",
+                style: TextStyle(fontSize: 22.0, letterSpacing: .5),
+              ),
+              accountEmail: Text(
+                'vl.martyniuk@gmail.com',
+                style: TextStyle(
+                  fontSize: 16.0,
+                ),
               ),
             ),
             ListTile(
-              title: Text('Save Animals'),
+              title: Text('Library'),
               onTap: () {
                 Navigator.push(
                   context,
@@ -60,133 +73,23 @@ class HomeScreen extends StatelessWidget {
               },
             ),
             ListTile(
-              title: Text('Bicycle App'),
+              title: Text('Basic'),
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => BicycleApp(),
+                    builder: (context) => SaveAnimals(),
                   ),
                 );
               },
             ),
             ListTile(
-              title: Text('Onboarding App'),
+              title: Text('Medium'),
               onTap: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => OnboardingScreens(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Story App'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => StoryApp(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Travel App - 2'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => Travel2App(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Restourant'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => RestaurantApp(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Smart House'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SmartHomeApp(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Travel Dark Theme'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DarkTravelApp(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Product Release'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => ProductReleaseApp(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Travel App'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TravelApp(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Bank App'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => BankScreen(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('TravelUI App'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TravelUIApp(),
-                  ),
-                );
-              },
-            ),
-            ListTile(
-              title: Text('Tattoo Studio'),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => TattooStudio(),
+                    builder: (context) => SaveAnimals(),
                   ),
                 );
               },
